af3fefba59a004213ec683ea7aec163a42ff8e1b
hangj <hangao1204@hotmail.com>
2022-07-20 20:14:47 +0800

    spring-boot-example-docker03 插件dockerfile-maven-plugins 打包

---
+++ spring-boot-example-docker/spring-boot-example-docker03/src/main/java/top/jackkke/DockerBuildApplication.java
@@ -0,0 +1,16 @@
+package top.jackkke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author jackkke
+ */
+@SpringBootApplication
+public class DockerBuildApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerBuildApplication.class, args);
+    }
+
+}
