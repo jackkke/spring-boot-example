b99b08ab4d89239d2a9cc418c3b47dcc2f7d8f24
hangj <hangao1204@hotmail.com>
2022-07-21 17:01:01 +0800

    spring-boot-example-docker04 jib打包

---
+++ spring-boot-example-docker/spring-boot-example-docker04/src/main/java/top/jackkke/web/DockerController.java
@@ -0,0 +1,20 @@
+package top.jackkke.web;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author jackkke
+ */
+@RestController
+public class DockerController {
+
+    @Value("${spring.application.name}")
+    private String name;
+
+    @GetMapping(value = "hello")
+    public Object sayHello(){
+        return "hello! I'm from " + name;
+    }
+}
