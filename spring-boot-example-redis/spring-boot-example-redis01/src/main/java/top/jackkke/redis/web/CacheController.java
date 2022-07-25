56e3980e936f73b04abc320c79abbc75a9a395e2
hangj <hangao1204@hotmail.com>
2022-07-25 10:22:06 +0800

    redis cache 自定义 #天/!小时

---
+++ spring-boot-example-redis/spring-boot-example-redis01/src/main/java/top/jackkke/redis/web/CacheController.java
@@ -0,0 +1,29 @@
+package top.jackkke.redis.web;
+
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author jackkke
+ */
+@RestController
+@CacheConfig(cacheNames = "jackkke")
+public class CacheController {
+
+    @GetMapping("day")
+    @Cacheable(value = "day#3", key = "#id")
+    public String day(@RequestParam(value = "id") String id) {
+        return id + "-" + LocalDateTime.now();
+    }
+
+    @GetMapping("hour")
+    @Cacheable(value = "hour!3", key = "#id")
+    public String hour(@RequestParam(value = "id") String id) {
+        return id + "-" + LocalDateTime.now();
+    }
+}
